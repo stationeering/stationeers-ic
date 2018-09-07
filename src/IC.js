c8842d85fb7802a0fea59bc2b6df81812c10fa40
Melair <melair@caldonia.net>
2018-09-07 09:03:00 +0100

    Strip old implementation out.

--- src/IC.js
+++ src/IC.js
@@ -1,34 +1,15 @@
 "use strict";
 
 const NEWLINE = "\n";
-const INSTRUCTION_SEPERATOR = " ";
+const INSTRUCTION_SEPERATOR = /\w+/;
+const COMMENT_SEPERATOR = "//";
 
 const INPUT_REGISTER_COUNT = 3;
 const OUTPUT_REGISTER_COUNT = 3;
 const INTERNAL_REGISTER_COUNT = 5;
-const MAXIMUM_INSTRUCTIONS = 10;
 
 const KNOWN_OPCODES = { 
-  "sel": 4,
-  "max": 3,
-  "min": 3,
-  "add": 3,
-  "sub": 3,
-  "mul": 3,
-  "div": 3,
-  "mod": 3,
-  "eq": 3,
-  "neq": 3,
-  "gt": 3,
-  "lt": 3,
-  "ceil": 2,
-  "flor": 2,
-  "abs": 2,
-  "log": 2,
-  "exp": 2,
-  "rou": 2,
-  "rand": 2,
-  "stor": 2
+
 };
 
 module.exports = class IC {  
@@ -38,127 +19,17 @@ module.exports = class IC {
     this._inputRegister = Array(INPUT_REGISTER_COUNT).fill(0);
     this._outputRegister = Array(OUTPUT_REGISTER_COUNT).fill(0);
     this._internalRegister = Array(INTERNAL_REGISTER_COUNT).fill(0);
-    this._literalRegister = [];
   }
 
   load(unparsedInstructions) {
-    let lineByLineInstructions = unparsedInstructions.split(NEWLINE);
-   
-    this._instructions = [];
-    this._literalRegister = [];
-    let errors = [];
+  }
 
-    for (const [i, instruction] of lineByLineInstructions.entries()) {
-      let trimmedInstruction = instruction.split("//", 1)[0].trim().toLowerCase();
+  validate() {
 
-      if (trimmedInstruction.length == 0) {
-        continue;
-      }
-
-      let splitInstruction = trimmedInstruction.split(INSTRUCTION_SEPERATOR);
-
-      let opCode = splitInstruction.shift();
-
-      let valid = true;
-
-      if (KNOWN_OPCODES.hasOwnProperty(opCode)) {
-        if (this._instructions.length >= MAXIMUM_INSTRUCTIONS) {
-          valid = false;
-          errors.push({ "line": i + 1, "error": "TOO_MANY_INSTRUCTIONS" });
-        }
-        
-        let fieldCount = KNOWN_OPCODES[opCode];
-
-        if (splitInstruction.length != fieldCount) {          
-          valid = false;
-          errors.push({ "line": i + 1, "error": "FIELD_COUNT_MISMATCH" });
-        } else {
-          for (const [j, field] of splitInstruction.entries()) {
-            let type = field.charAt(0);
-
-            switch(type) {              
-            case "i":          
-              if (j == (fieldCount - 1)) {
-                valid = false;
-                errors.push({ "line": i + 1, "error": "READ_ONLY_REGISTER", "field": j });
-              }
-              // falls through
-
-            case "r":
-            case "o":
-              var stringNumber = field.slice(1);
-              var number = parseInt(stringNumber);
-              var maxValue = 0;
-
-              switch(type) {
-              case "i":
-                maxValue = INPUT_REGISTER_COUNT;
-                break;
-              case "o":
-                maxValue = OUTPUT_REGISTER_COUNT;
-                break;
-              case "r":
-                maxValue = INTERNAL_REGISTER_COUNT;
-                break;
-              }
-
-              if (number > (maxValue - 1) || stringNumber.length == 0) {
-                valid = false;
-                errors.push({ "line": i + 1, "error": "OUT_OF_BOUND_REGISTER", "field": j });
-              }
-              break;
-
-            default:
-              var parsedField = Number.parseFloat(field);
-
-              if (!Number.isNaN(parsedField)) {
-                if (j == (fieldCount - 1)) {
-                  valid = false;
-                  errors.push({ "line": i + 1, "error": "READ_ONLY_REGISTER", "field": j });
-                } else {
-                  let newSize = this._literalRegister.push(parsedField);
-                  splitInstruction[j] = `l${newSize - 1}`;
-                }
-              } else {
-                valid = false;
-                errors.push({ "line": i + 1, "error": "UNKNOWN_REGISTER", "field": j });  
-              }
-            }
-          }          
-        }     
-      } else {
-        valid = false;
-        errors.push({ "line": i + 1, "error": "UNKNOWN_INSTRUCTION" });
-      }
-
-      if (valid) {
-        this._instructions.push({ "opCode": opCode, "fields": splitInstruction });
-      }
-    }
-
-    return errors;
   }
 
   instructionCount() {
     return this._instructions.length;
-  }
-
-  getInstructions() {
-    return this._instructions.map((instruction) => {
-      let result = [];
-      result[0] = instruction.opCode.toUpperCase();
-      
-      let demapped = instruction.fields.map((field) => {
-        if (field.charAt(0) === "l") {
-          return this._getRegister(field).toString();
-        }
-        return field;
-      });
-
-      result = result.concat(demapped);
-      
-      return result;
-    });
   }
 
   getInputRegisters() {
@@ -220,15 +91,15 @@ module.exports = class IC {
       return this.getInternalRegisters()[number];
     case "o":
       return this.getOutputRegisters()[number];
-    case "l":
-      return this._literalRegister[number];
     }
   }
 
-  step() {    
-    this._execute(this._instructions[this._programCounter]);
+  step() {        
+    var instruction = this._instructions[this._programCounter];
     this._programCounter++;
 
+    this._execute(instruction);
+    
     return this._programCounter < this.instructionCount();
   }
 
@@ -237,165 +108,5 @@ module.exports = class IC {
   }
 
   _execute(instruction) {
-    switch(instruction["opCode"]) {
-    case "sel":
-      return this._execute_sel(instruction["fields"]);
-
-    case "max":
-      return this._execute_max(instruction["fields"]);
-    case "min":
-      return this._execute_min(instruction["fields"]);
-
-    case "add":
-      return this._execute_add(instruction["fields"]);
-    case "sub":
-      return this._execute_sub(instruction["fields"]);
-    case "mul":
-      return this._execute_mul(instruction["fields"]);
-    case "div":
-      return this._execute_div(instruction["fields"]);
-    case "mod":
-      return this._execute_mod(instruction["fields"]);
-
-    case "eq":
-      return this._execute_eq(instruction["fields"]);
-    case "neq":
-      return this._execute_neq(instruction["fields"]);         
-    case "gt":
-      return this._execute_gt(instruction["fields"]);     
-    case "lt":
-      return this._execute_lt(instruction["fields"]);     
-
-    case "ceil":
-      return this._execute_ceil(instruction["fields"]);     
-    case "flor":
-      return this._execute_flor(instruction["fields"]);     
-    case "abs":
-      return this._execute_abs(instruction["fields"]);     
-    case "log":
-      return this._execute_log(instruction["fields"]);     
-    case "exp":
-      return this._execute_exp(instruction["fields"]);     
-    case "rou":
-      return this._execute_rou(instruction["fields"]);     
-    case "rand":
-      return this._execute_rand(instruction["fields"]);              
-    case "stor":
-      return this._execute_stor(instruction["fields"]);           
-    }
   }
-
-  _execute_sel(fields) {  
-    let compared = this._getRegister(fields[0]);
-    let valueRegister = (compared < 1.0 ? fields[1] : fields[2]);
-    this._setRegister(fields[3], this._getRegister(valueRegister));
-  }
-
-  _execute_max(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    let maxValue = Math.max(valueOne, valueTwo);
-    this._setRegister(fields[2], maxValue);
-  }  
-
-  _execute_min(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    let minValue = Math.min(valueOne, valueTwo);
-    this._setRegister(fields[2], minValue);
-  } 
-  
-  _execute_add(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne + valueTwo);
-  } 
-
-  _execute_sub(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne - valueTwo);
-  } 
-
-  _execute_mul(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne * valueTwo);
-  } 
-
-  _execute_div(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne / valueTwo);
-  } 
-
-  _execute_mod(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne % valueTwo);
-  } 
-
-  _execute_eq(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne == valueTwo ? 1 : 0);
-  } 
-
-  _execute_neq(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne != valueTwo ? 1 : 0);
-  } 
-
-  _execute_gt(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne > valueTwo ? 1 : 0);
-  } 
-
-  _execute_lt(fields) {  
-    let valueOne = this._getRegister(fields[0]);
-    let valueTwo = this._getRegister(fields[1]);
-    this._setRegister(fields[2], valueOne < valueTwo ? 1 : 0);
-  } 
-
-  _execute_ceil(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], Math.ceil(value));
-  } 
-
-  _execute_flor(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], Math.floor(value));
-  } 
-
-  _execute_abs(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], Math.abs(value));
-  } 
-
-  _execute_log(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], Math.log(value));
-  } 
-
-  _execute_exp(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], Math.exp(value));
-  } 
-
-  _execute_rou(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], Math.round(value));
-  } 
-
-  _execute_rand(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], Math.random() * value);
-  } 
-  
-  _execute_stor(fields) {  
-    let value = this._getRegister(fields[0]);
-    this._setRegister(fields[1], value);
-  } 
 };
--- test/IC.test.js
+++ test/IC.test.js
@@ -5,184 +5,9 @@ const expect = require("chai").expect;
 const IC = require("../src/IC");
 
 describe("IC Tests", function () {
-  const VALID_SINGLE_INSTRUCTION = "SEL i0 i1 i2 r0";
-  const VALID_MULTIPLE_INSTRUCTION = "SEL i0 i1 i2 r0\nMAX i0 i1 r2";
-  const VALID_MULTIPLE_INSTRUCTION_WITH_COMMENT = "SEL i0 i1 i2 r0 // Comment\nMAX i0 i1 r2";
-  const VALID_MULTIPLE_INSTRUCTION_WITH_COMMENT_ON_OWN_LINE = "SEL i0 i1 i2 r0\n // Comment\nMAX i0 i1 r2";
-  const INVALID_SINGLE_INSTRUCTION = "INVALID i0 i1 r2";
-  const INVALID_SINGLE_INSTRUCTION_MISS_FIELD = "SEL i0 i1 i2";
-  const INVALID_SINGLE_INSTRUCTION_INVALID_FIELD_NO_SUCH_INPUT = "SEL i5 i1 i2 o1";
-  const INVALID_SINGLE_INSTRUCTION_INVALID_FIELD_UNKNOWN_REGISTER = "SEL u0 i1 i2 o1";
-  const INVALID_SINGLE_INSTRUCTION_INVALID_FIELD_WRITING_TO_INPUT = "SEL i0 i1 i2 i1";
-  const INVALID_SINGLE_INSTRUCTION_SHORT_REGISTER = "SEL i0 i1 i2 o";
+  const VALID_MULTIPLE_INSTRUCTION = "YIELD\nYIELD\nYIELD\n";
 
   describe("Loading instructions", function () {
-    it("loads a valid instruction and returns an empty error array", function () {
-      let ic = new IC();
-
-      let result = ic.load(VALID_SINGLE_INSTRUCTION);
-      let instructionCount = ic.instructionCount();
-
-      expect(result.length).to.equal(0);
-      expect(instructionCount).to.equal(1);
-    });
-
-    it("loads a valid set of instructions and returns an empty error array", function () {
-      let ic = new IC();
-
-      let result = ic.load(VALID_MULTIPLE_INSTRUCTION);
-      let instructionCount = ic.instructionCount();
-
-      expect(result.length).to.equal(0);
-      expect(instructionCount).to.equal(2);
-    });
-
-    it("loads a valid set of instructions with comment and returns an empty error array", function () {
-      let ic = new IC();
-
-      let result = ic.load(VALID_MULTIPLE_INSTRUCTION_WITH_COMMENT);
-      let instructionCount = ic.instructionCount();
-
-      expect(result.length).to.equal(0);
-      expect(instructionCount).to.equal(2);
-    });
-
-    it("loads a valid set of instructions with comment and returns an empty error array", function () {
-      let ic = new IC();
-
-      let result = ic.load(VALID_MULTIPLE_INSTRUCTION_WITH_COMMENT_ON_OWN_LINE);
-      let instructionCount = ic.instructionCount();
-
-      expect(result.length).to.equal(0);
-      expect(instructionCount).to.equal(2);
-    });
-
-    it("loads a invalid set of instructions and returns one error describing the fault", function () {
-      let ic = new IC();
-
-      let result = ic.load(INVALID_SINGLE_INSTRUCTION);
-
-      expect(result.length).to.equal(1);
-
-      expect(result[0]["line"]).to.equal(1);
-      expect(result[0]["error"]).to.equal("UNKNOWN_INSTRUCTION");
-    });
-
-    it("loads and verifies that opcode needs specific number of fields", function () {
-      let ic = new IC();
-
-      let result = ic.load(INVALID_SINGLE_INSTRUCTION_MISS_FIELD);
-
-      expect(result.length).to.equal(1);
-
-      expect(result[0]["line"]).to.equal(1);
-      expect(result[0]["error"]).to.equal("FIELD_COUNT_MISMATCH");
-    });
-
-    it("only load the first 10 instructions", function () {
-      let ic = new IC();
-
-      let elevenInstructions = Array(11).fill(VALID_SINGLE_INSTRUCTION).join("\n");
-
-      let result = ic.load(elevenInstructions);
-
-      expect(result.length).to.equal(1);
-      expect(result[0]["line"]).to.equal(11);
-      expect(result[0]["error"]).to.equal("TOO_MANY_INSTRUCTIONS");
-    });
-
-
-    it("verifies that reads can't reference register out side of range", function () {
-      let ic = new IC();
-
-      let result = ic.load(INVALID_SINGLE_INSTRUCTION_INVALID_FIELD_NO_SUCH_INPUT);
-
-      expect(result.length).to.equal(1);
-
-      expect(result[0]["line"]).to.equal(1);
-      expect(result[0]["error"]).to.equal("OUT_OF_BOUND_REGISTER");
-      expect(result[0]["field"]).to.equal(0);
-    });
-
-
-    it("verifies that a register must have a number otherwise it's out of bound", function () {
-      let ic = new IC();
-
-      let result = ic.load(INVALID_SINGLE_INSTRUCTION_SHORT_REGISTER);
-
-      expect(result.length).to.equal(1);
-
-      expect(result[0]["line"]).to.equal(1);
-      expect(result[0]["error"]).to.equal("OUT_OF_BOUND_REGISTER");
-      expect(result[0]["field"]).to.equal(3);
-    });
-
-    it("verifies that reads can't reference unknown register type", function () {
-      let ic = new IC();
-
-      let result = ic.load(INVALID_SINGLE_INSTRUCTION_INVALID_FIELD_UNKNOWN_REGISTER);
-
-      expect(result.length).to.equal(1);
-
-      expect(result[0]["line"]).to.equal(1);
-      expect(result[0]["error"]).to.equal("UNKNOWN_REGISTER");
-      expect(result[0]["field"]).to.equal(0);
-    });
-
-    it("verifies that writes can't go to input registers", function () {
-      let ic = new IC();
-
-      let result = ic.load(INVALID_SINGLE_INSTRUCTION_INVALID_FIELD_WRITING_TO_INPUT);
-
-      expect(result.length).to.equal(1);
-
-      expect(result[0]["line"]).to.equal(1);
-      expect(result[0]["error"]).to.equal("READ_ONLY_REGISTER");
-      expect(result[0]["field"]).to.equal(3);
-    });
-  });
-
-  describe("Literal registers", function () {
-    it("can handle a literal in place of a register for non output field", function () {
-      let ic = new IC();
-
-      let result = ic.load("ADD 1.2 1.5 r0");
-      
-      expect(result.length).to.equal(0);
-
-      ic.step();
-
-      expect(ic.getInternalRegisters()[0]).to.equal(2.7);
-    });
-
-    it("verifies that literal registers can not be used for an output", function () {
-      let ic = new IC();
-
-      let result = ic.load("ADD i0 i1 2.2");
-
-      expect(result.length).to.equal(1);
-
-      expect(result[0]["line"]).to.equal(1);
-      expect(result[0]["error"]).to.equal("READ_ONLY_REGISTER");
-      expect(result[0]["field"]).to.equal(2);
-    });
-  });
-
-  describe("Returns instructions", function () {
-    it("including those with literals correctly", function () {
-      let ic = new IC();
-
-      let result = ic.load("ADD 1.2 1.5 r0\nSUB i0 1.5 r0");
-
-      expect(result.length).to.equal(0);
-
-      let instructions = ic.getInstructions();
-
-      expect(instructions.length).to.equal(2);
-
-      expect(instructions[0]).to.eql(["ADD", "1.2", "1.5", "r0"]);
-      expect(instructions[1]).to.eql(["SUB", "i0", "1.5", "r0"]);
-    });
   });
 
   describe("Inputs, outputs and registers", function () {
@@ -241,14 +66,14 @@ describe("IC Tests", function () {
   });
 
   describe("Step and flow behaviour", function () {
-    it("increases the programme counter to increase and return true when there are more instructions to execute", function () {
+    it("increases the programme counter to increase", function () {
       let ic = new IC();
       ic.load(VALID_MULTIPLE_INSTRUCTION);
 
       expect(ic.programCounter()).to.equal(0);
-      expect(ic.step()).to.equal(true);
+      ic.step();
       expect(ic.programCounter()).to.equal(1);
-      expect(ic.step()).to.equal(false);
+      ic.step();
       expect(ic.programCounter()).to.equal(2);
     });
 
